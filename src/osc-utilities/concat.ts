1c6d9620f1be251a6058aa5526bd218aa39fd425
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-07 23:49:32 +0100

    TypeScript rewrite of `concat` in `lib/osc-utilities.coffee`

---
+++ src/osc-utilities/concat.ts
@@ -0,0 +1,36 @@
+/**
+ * Utility for working with buffers.
+ *
+ * Takes an array of buffers, output one buffer with all of the array concatenated.
+ *
+ * This is really only exported for TDD, but maybe it'll be useful to someone else too.
+ *
+ * @param {Buffer[]} buffers
+ * @returns {Buffer[]}
+ */
+export function concat(buffers: Buffer[]): Buffer {
+  if (!Array.isArray(buffers)) {
+    throw new Error("Concat must take an array of buffers.");
+  }
+
+  for (const buffer of buffers) {
+    if (!Buffer.isBuffer(buffer)) {
+      throw new Error("Concat must take an array of buffers.");
+    }
+  }
+
+  let sumLength = 0;
+  for (const buffer of buffers) {
+    sumLength += buffer.length;
+  }
+
+  const destBuffer = Buffer.alloc(sumLength);
+
+  let copyTo = 0;
+  for (const buffer of buffers) {
+    buffer.copy(destBuffer, copyTo);
+    copyTo += buffer.length;
+  }
+
+  return destBuffer;
+}
