28ac07ed15973dfe192d726fc580e8ebba87efa1
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-08 12:11:42 +0100

    Refactor variable name in timestampToTimetag function

--- src/osc-utilities/timestampToTimetag.ts
+++ src/osc-utilities/timestampToTimetag.ts
@@ -3,11 +3,11 @@ import type { Timetag } from "src/types";
 
 /**
  * Convert a unix timestamp (seconds since Jan 1 1970 UTC) to NTP timestamp array.
- * @param {number} secs The unix timestamp in seconds.
+ * @param {number} seconds The unix timestamp in seconds.
  * @returns {number[]} The NTP timestamp array.
  */
-export function timestampToTimetag(secs: number): Timetag {
-  const wholeSecs: number = Math.floor(secs);
-  const fracSeconds: number = secs - wholeSecs;
+export function timestampToTimetag(seconds: number): Timetag {
+  const wholeSecs: number = Math.floor(seconds);
+  const fracSeconds: number = seconds - wholeSecs;
   return makeTimetag(wholeSecs, fracSeconds);
 }
