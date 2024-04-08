420c24a43ef1b9dc45d8dc6dc4d2a0337e9fb946
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-08 11:53:11 +0100

    Update JSDoc

--- src/osc-utilities/dateToTimetag.ts
+++ src/osc-utilities/dateToTimetag.ts
@@ -6,7 +6,7 @@ import { timestampToTimetag } from "src/osc-utilities";
  * Time zone of the Date object is respected, as the NTP timetag uses UTC.
  *
  * @param {Date} date - The JavaScript Date object to convert.
- * @returns {number[]} - The NTP timetag array.
+ * @returns {number[]} The NTP timetag array.
  */
 export function dateToTimetag(date: Date): number[] {
   return timestampToTimetag(date.getTime() / 1000);
