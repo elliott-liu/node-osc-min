ae10332059f3c20fd8967896c02142cf46468559
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-08 14:58:51 +0100

    Implement `isOscBundleBuffer`

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,3 +2,4 @@ export * from "src/common/StrictError";
 export * from "src/common/padding";
 export * from "src/common/makeTimetag";
 export * from "src/common/constants";
+export * from "src/common/isOscBundleBuffer";
---
+++ src/common/isOscBundleBuffer.ts
@@ -0,0 +1,15 @@
+import { splitOscString } from "src/osc-utilities";
+
+/**
+ * Internal function to check if this is a message or bundle.
+ *
+ * @param buffer - The buffer to check.
+ * @param strict - Whether to perform strict checking.
+ * @returns `true` if the buffer represents a bundle, `false` otherwise.
+ */
+export function isOscBundleBuffer(buffer: Buffer, strict: boolean): boolean {
+  // Both formats begin with strings, so we should just grab the front but not consume it.
+  const { string } = splitOscString(buffer, strict);
+
+  return string === "#bundle";
+}
