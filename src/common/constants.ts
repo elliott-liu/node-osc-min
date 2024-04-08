7c7317ca82ddb8b59ff3e49692c67ca44b4a08a2
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-08 11:41:48 +0100

    Add comments for TWO_POW_32 value

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,3 @@
 // NTP epoch is 1900, JavaScript Date is Unix 1970
 export const UNIX_EPOCH = 2208988800;
-export const TWO_POW_32 = Math.pow(2, 32);
+export const TWO_POW_32 = Math.pow(2, 32); // 4294967296
