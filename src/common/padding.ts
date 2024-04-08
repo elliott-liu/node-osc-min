8808205953feb573cfbdf3d95465d2ac4640aa12
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-08 17:15:03 +0100

    Refactor padding function parameter name for clarity, update JSDoc

--- src/common/padding.ts
+++ src/common/padding.ts
@@ -1,10 +1,10 @@
 /**
  * Calculate the padding required for an osc-string.
  *
- * @param str - The osc-string.
+ * @param string - The osc-string.
  * @returns The padding required for the osc-string.
  */
-export function padding(str: string): number {
-  const length = Buffer.byteLength(str);
+export function padding(string: string): number {
+  const length = Buffer.byteLength(string);
   return (4 - (length % 4)) % 4;
 }
