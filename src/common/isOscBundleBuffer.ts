e6c17bdbdbd65bc5d9f20c2bbd82e134e19d9d22
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-08 16:51:06 +0100

    Add default for `strict` parameter

--- src/common/isOscBundleBuffer.ts
+++ src/common/isOscBundleBuffer.ts
@@ -7,7 +7,10 @@ import { splitOscString } from "src/osc-utilities";
  * @param strict - Whether to perform strict checking.
  * @returns `true` if the buffer represents a bundle, `false` otherwise.
  */
-export function isOscBundleBuffer(buffer: Buffer, strict: boolean): boolean {
+export function isOscBundleBuffer(
+  buffer: Buffer,
+  strict: boolean = false,
+): boolean {
   // Both formats begin with strings, so we should just grab the front but not consume it.
   const { string } = splitOscString(buffer, strict);
 
