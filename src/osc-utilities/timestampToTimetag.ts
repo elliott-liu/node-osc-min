bc9a76b6cef30c04fa104238c86ead242ff210cc
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-08 12:11:22 +0100

    Use `Timetag` as return type

--- src/osc-utilities/timestampToTimetag.ts
+++ src/osc-utilities/timestampToTimetag.ts
@@ -1,11 +1,12 @@
 import { makeTimetag } from "src/common";
+import type { Timetag } from "src/types";
 
 /**
  * Convert a unix timestamp (seconds since Jan 1 1970 UTC) to NTP timestamp array.
  * @param {number} secs The unix timestamp in seconds.
  * @returns {number[]} The NTP timestamp array.
  */
-export function timestampToTimetag(secs: number): number[] {
+export function timestampToTimetag(secs: number): Timetag {
   const wholeSecs: number = Math.floor(secs);
   const fracSeconds: number = secs - wholeSecs;
   return makeTimetag(wholeSecs, fracSeconds);
