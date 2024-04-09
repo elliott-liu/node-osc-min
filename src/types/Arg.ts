da515ad97b7e515c3f9a8745e23f31ca38b4a657
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-09 15:00:22 +0100

    Add `Arg` types and export

---
+++ src/types/Arg.ts
@@ -0,0 +1,51 @@
+export type Arg =
+  | {
+      type: "string";
+      value: string;
+    }
+  | {
+      type: "integer";
+      value: number;
+    }
+  | {
+      type: "timetag";
+      value: string;
+    }
+  | {
+      type: "float";
+      value: number;
+    }
+  | {
+      type: "double";
+      value: number;
+    }
+  | {
+      type: "blob";
+      value: Buffer;
+    }
+  | {
+      type: "true";
+      value: true;
+    }
+  | {
+      type: "false";
+      value: false;
+    }
+  | {
+      type: "null";
+      value: null;
+    }
+  | {
+      type: "bang";
+      value: "bang";
+    };
+
+export type ArgRepresentation = Extract<Arg, {}>["type"];
+export type ArgValue = Extract<Arg, {}>["value"];
+
+export type OscMessageArg =
+  | Arg
+  | {
+      type: "array";
+      value: OscMessageArg[] | undefined;
+    };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "src/types/BufferType";
 export * from "src/types/Timetag";
 export * from "src/types/BundleType";
+export * from "src/types/Arg";
