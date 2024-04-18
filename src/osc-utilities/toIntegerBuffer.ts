3bc9cfbe47625eec9304d225898ba6f5c33a9154
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-04-18 20:54:31 +0800

    Refactor toIntegerBuffer function to support Int64 and UInt64 types

--- src/osc-utilities/toIntegerBuffer.ts
+++ src/osc-utilities/toIntegerBuffer.ts
@@ -17,31 +17,48 @@ export function toIntegerBuffer(
   if (typeof number !== "number") {
     throw new Error("Cannot pack a non-number into an integer buffer");
   }
-  const buffer = Buffer.alloc(4);
+
+  let buffer: Buffer;
 
   switch (type) {
     case "Float64":
+      buffer = Buffer.alloc(8);
       buffer.writeDoubleBE(number, 0);
       break;
     case "Float32":
+      buffer = Buffer.alloc(4);
       buffer.writeFloatBE(number, 0);
       break;
+    case "Int64":
+      buffer = Buffer.alloc(8);
+      buffer.writeBigInt64BE(BigInt(number), 0);
+      break;
     case "Int32":
+      buffer = Buffer.alloc(4);
       buffer.writeInt32BE(number, 0);
       break;
     case "Int16":
+      buffer = Buffer.alloc(2);
       buffer.writeInt16BE(number, 0);
       break;
     case "Int8":
+      buffer = Buffer.alloc(1);
       buffer.writeInt8(number, 0);
       break;
+    case "UInt64":
+      buffer = Buffer.alloc(8);
+      buffer.writeBigUInt64BE(BigInt(number), 0);
+      break;
     case "UInt32":
+      buffer = Buffer.alloc(4);
       buffer.writeUInt32BE(number, 0);
       break;
     case "UInt16":
+      buffer = Buffer.alloc(2);
       buffer.writeUInt16BE(number, 0);
       break;
     case "UInt8":
+      buffer = Buffer.alloc(1);
       buffer.writeUInt8(number, 0);
       break;
     default:
